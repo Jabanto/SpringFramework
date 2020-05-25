8b3bb13e5aeff935ede045b9ac82fc45023b6cda
HugoAbanto <HugoAbanto@bitbucket.org>
2020-05-25 21:38:31 +0200

    new psirng boot porject

--- SpringCurso/Spring_MVC_Ejemplo1/src/main/WebContent/WEB-INF/vista/paginaEjemplo.jsp
+++ SpringCurso/Spring_MVC_Ejemplo1/src/main/WebContent/WEB-INF/vista/paginaEjemplo.jsp
@@ -1,7 +1,12 @@
-Peticion URL
-
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core
+"
+         prefix="c" %>
+<%@ taglib uri="/functions" prefix="f" %>
 <html>
-
-<a href="muestraFormulario"></a>
-
+<head><title>Localized Dates</title></head>
+<body bgcolor="white">
+       <h1> Primer Ejemplo deMVC con Spring </h1>
+       < a href="muestraFormulario">Ir al Formulario o metodo muestraFormulario definido en el controller</a>
+</body>
 </html>
--- SpringCurso/Spring_MVC_Ejemplo1/src/main/java/es/pildoras/spring/mvc/HolaAlunnosControlador.java
+++ SpringCurso/Spring_MVC_Ejemplo1/src/main/java/es/pildoras/spring/mvc/HolaAlunnosControlador.java
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HolaAlunnosControlador {
 
-        //Metodo para pedir el formulario, proporciona el formulario
-        // peduirle al servidor el formulario pedido
-        // Peticion de una URL (especificamos la url que tiene que utilizar el usuario para que servidor le devuelva el formulario)
-        // normalmente se utiliza la url el nombre del metodo aunque no siempre
+        // Metodo para pedir el formulario, proporciona el formulario
+        // o Pedirle al servidor el formulario pedido
+        // RequestMapping tiene una pararemtro  que es una peticion de una URL (especificamos la url que tiene que utilizar el usuario para que servidor le devuelva el formulario)
+        // normalmente pra hacer el nombre de la url se utiliza el nombre del metodo aunque no siempre
+        // RequestMapping lo podemos traducirlo como peticion de url
         @RequestMapping("/muestraFormulario")
         public String muestraFormulario(){
 
@@ -17,7 +18,8 @@ public class HolaAlunnosControlador {
         }
 
 
-        // y otro metodo que sea capaz de trabajar/procesa lainformcion del formulario cuandoesta sea enviada
+        // y otro metodo que sea capaz de trabajar/procesar la informcion del formulario cuando esta sea enviada
+        // una vez procesado se enviara un JSP /html como respuesta a la view HolaAlumnosSpring
         @RequestMapping("/procesarFormulario")
         public String procesarFormulario(){
 
